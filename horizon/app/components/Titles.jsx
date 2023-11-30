c17e8758eb29e5898be79b0d14139c73d8b3f885
Barba <patrickamc@protonmail.com>
2023-11-30 15:47:59 -0300

    CheckoutPage

--- horizon/app/components/Titles.jsx
+++ horizon/app/components/Titles.jsx
@@ -33,17 +33,24 @@ const Titles = () => {
         return {
           _titleId: eventData?._titleId?.toString() ?? "N/A",
           _scheduleId: eventData?._scheduleId?.toString() ?? "N/A",
-          _monthlyValue: eventData?._monthlyValue?.toString() ?? "N/A",
-          _titleValue: eventData?._titleValue?.toString() ?? "N/A",
+          _monthlyValue:
+            parseFloat(eventData?._monthlyValue?.toString()) / 10 ** 18 ??
+            "N/A",
+          _titleValue:
+            parseFloat(eventData?._titleValue?.toString()) / 10 ** 18 ?? "N/A",
         };
       });
       setTitles(formattedEvents);
-
     } else {
       setTitles([]);
     }
     setIsLoading(false);
   }, [events]);
+
+  const convertWeiToDollar = (wei) => {
+    const etherValue = wei;
+    return parseFloat(etherValue) / 10 ** 18;
+  };
 
   return (
     <main className="mt-5">
@@ -64,20 +71,21 @@ const Titles = () => {
             key={event._titleId}
             className="card card-side bg-base-100 shadow-xl mt-5"
           >
-            <figure>
+            <figure className="justify-items-start lg:max-h-96 lg:max-w-96">
               <img
                 src={titleImages[index % titleImages.length]}
                 alt={`Title ${event._titleId}`}
               />
             </figure>
-            <div className="card-body">
+            <div className="card-body place-content-center">
               <h2 className="card-title">Title ID: {event._titleId}</h2>
               <ul>
-                <li>Value: {event._titleValue}</li>
-                <li>Monthly Value: {event._monthlyValue}</li>
+                <li>Schedule: {event._scheduleId}</li>
+                <li>Value: ${event._titleValue}.00</li>
+                <li>Monthly Value: ${event._monthlyValue}.00</li>
                 <li>Installments: {event._installments}</li>
               </ul>
-              <div className="card-actions justify-end">
+              <div className="card-actions">
                 <Link href={`/allproducts/${event._titleId}`}>
                   <button className="btn btn-accent text-base-100">
                     More info
